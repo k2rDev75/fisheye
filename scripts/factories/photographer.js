96dec2c6c99b6563fc603d332f625df821c78aea
abdelqader <abdelqader@MBP-de-michael.lan>
2023-01-30 18:23:29 +0100

    Ajout de la page une

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
+  console.log(name, id, city, country, tagline, price, portrait);
+  const picture = `assets/photographersId/${portrait}`;
 
-    const picture = `assets/photographers/${portrait}`;
+  function getUserCardDOM() {
+    let article = document.createElement("article");
+    article.classList.add("card");
+    article.innerHTML = `
+            <a href="photographer.html?photographer=${id}", aria-label="Page de ${name}"/>
+                <img src="assets/photographersId/${portrait}" alt="Photo de profil du photographe ${name}">
+                <h2>${name}</h2>
+            </a>
+            <p class="location">${city}, ${country}</p>
+            <p class="tagline">${tagline}</p>
+            <p class="price">${price}€/jour</p>
+        `;
+    return article;
+  }
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function headerPhotographerDOM() {
+    let article = document.createElement("article");
+    article.classList.add("header_profil");
+    article.innerHTML = `
+            <div id="photographer-details">
+                <h1>${name}</h1>
+                <p id="location">${city}, ${country}</p>
+                <p id="tagline">${tagline}</p>
+            </div>
+            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+            <img src="assets/photographersId/${portrait}" alt="Photo de profil du photographe ${name}">
+            `;
+    return article;
+  }
+
+  return { getUserCardDOM, headerPhotographerDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,41 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const photographerID = () =>
+  new URLSearchParams(window.location.search).get("photographer");
+// console.log("🚀 ~ file: photographer.js:3 ~ photographerID", photographerID())
+const id = photographerID();
+
+async function getAllData() {
+  try {
+    const getData = await fetch("../../data/photographers.json");
+    const data = await getData.json();
+    // console.log("🚀 ~ file: photographer.js:11 ~ getAllData ~ data", data);
+
+    return data;
+  } catch (error) {
+    console.log("Error =>: ", error);
+  }
+}
+// récupération des ID pour les photographers et les medias
+async function photographersById() {
+  const { photographers } = await getAllData();
+  const photographerByID = photographers.find((elem) => id == elem.id);
+  return photographerByID;
+}
+let photoById = photographersById();
+async function mediasById() {
+  const { media } = await getAllData();
+  const allMediaById = media.filter((elem) => id == elem.photographerId);
+  return allMediaById;
+}
+
+/*****************************************************/
+
+async function displayProfilHeader(photographer) {
+  const header = document.querySelector(".photograph-header");
+  const photographerModel = photographerFactory(photographer);
+  const userCardDOM = photographerModel.headerPhotographerDOM();
+  header.append(userCardDOM);
+}
+mediasById();
+photographersById();
+displayProfilHeader(photographersById());
