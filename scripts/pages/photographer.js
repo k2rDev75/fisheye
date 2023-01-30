384b820d8c35e05ea68998637af91ae8b1ca2a64
abdelqader <abdelqader@MBP-de-michael.lan>
2023-01-30 21:26:05 +0100

    effacement de la function init

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -13,14 +13,11 @@ async function getAllData() {
     console.log("Error =>: ", error);
   }
 }
+
 // récupération des ID pour les photographers et les medias
-async function photographersById(photographer) {
+async function photographersById() {
   const { photographers } = await getAllData();
   const photographerByID = photographers.find((elem) => id == elem.id);
-  console.log(
-    "🚀 ~ file: photographer.js:22 ~ photographersById ~ photographerByID",
-    photographerByID
-  );
   return photographerByID;
 }
 
@@ -35,14 +32,10 @@ async function mediasById() {
 async function displayProfilHeader(photographer) {
   const header = document.querySelector(".photograph-header");
   const photographerModel = photographerFactory(photographer);
-  const userCardDOM = photographerModel.headerPhotographerDOM();
-  header.append(userCardDOM);
+  const photographerHeaderById = photographerModel.headerPhotographerDOM();
+  header.append(photographerHeaderById);
 }
-async function init() {
-  const { photographers, media } = await getAllData();
-  console.log("🚀 ~ file: photographer.js:43 ~ init ~ photographers", photographers)
-  photographersById();
-  mediasById();
-  displayProfilHeader(photographers);
-}
-init();
+
+photographersById();
+mediasById();
+displayProfilHeader(getAllData());
